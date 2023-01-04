8dca4f7ad72a5ff44d034366421a0b2a2a1d1603
Zacharia <ndunguzachary24@gmail.com>
2023-01-04 11:48:35 +0300

    Product Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :image, presence: true
+    validates :price, presence: true
 end
