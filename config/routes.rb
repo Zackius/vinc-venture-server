c3c62927c26f2880139bbdcce35909d1e79263ab
Zacharia <ndunguzachary24@gmail.com>
2023-01-04 12:00:14 +0300

    Product/Category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories, only:[:index, show, :create. :]
+  resources :products, only: [:index, :show, :create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
