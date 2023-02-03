83d0d15782d5e785c7f0c7e1c92b80b6d4ce457d
Zacharia <ndunguzachary24@gmail.com>
2023-02-03 12:54:17 +0300

    Include 2 origins in core

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://vincventures.co.ke"
+    origins "https://vincventures.co.ke, http://localhost:3000/"
 
     resource "*",
       headers: :any,
