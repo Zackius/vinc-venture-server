e65dc8e0a2509258961c8a58d313537af7076e80
Zacharia <ndunguzachary24@gmail.com>
2023-09-04 17:10:54 +0300

    Configured Render on the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,94 @@ Category.create(name: 'Nunix')
 Category.create(name: 'Dumbbells')
 Category.create(name: 'Gym & Fitness')
 Category.create(name: 'Bedroom')
-Product.create(name: "12 Pcs Signature Cookware Set", description: "Suitable to cook any recipe and selected to handle a large range of kitchen tasks. Exceptionally durable dishwasher safe cookware for easy cleanup. Durable non-stick interior for easy cleaning and cooking. The set is superior scratch resistance and has a total non-stick interior that provides high performance and cooking experience. Designed for high performance and durability, but also to look great in any kitchen. Ergonomic comfortable grip.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/wawaw.jpg" , price: "6,500", category_id: 1 )
-Product.create(name: "3-tier fruit bowl/ candy bowl/ Snack bowl", description: "Ceramic candy dish living room home three-layer fruit plate snack plate creative modern dried fruit fruit basket.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/3-tier-fruit-bowl-candy-bowl-Snack-bowl-1.jpg" , price: "4,500", category_id: 1 )
-Product.create(name: "Always Stainless Steel Vacuum Flask", description: "Stainless steel insulation hence unbreakable.Durable and easy to wash.Easy to carry anywhere & you can use it to serve guests or anyone.Perfect to use in offices, homes, restaurants, schools, churches etc.The flask can also be used to keep hot anything-Hot chocolate, coffee, tea, Uji etc.The flask keeps items hot  for more than 24 hours.The product is a guarantee customer satisfaction", image: "https://decorfinity.co.ke/wp-content/uploads/2021/03/Flask-2-e1614669189372.jpg" , price: "2,500", category_id: 1 )
-Product.create(name: "Bosch Cookware",description: "Since It’s Non-Stick, You get to use less oil – this means healthier meals – when you cook with the Bosch Non-stick Cooking Pots, the granite coating does not come out when heated or gently scratched.", image: "https://goldstarnovelty.co.ke/wp-content/uploads/2022/03/1-1-600x600-1.png" , price: "10,500", category_id: 1 )
-Product.create(name: "9PCs Knife Set-Stainless Steel", description: "These knives will do wonders for your life. Stored on a gorgeous acrylic stand and equipped with ergonomic soft touch handles, this knife sets are a beautiful compliment to any kitchen ", image: "https://decorfinity.co.ke/wp-content/uploads/2022/05/9-13-600x800.jpg" , price: "10,500", category_id: 1 )
-Product.create(name: "6pc Coffee Mug Set With Bamboo Stand", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/cpp.jpg" , price: "3000", category_id: 1 )
-Product.create(name: "30 Pcs Stainless Steel Non-Stick Cookware Set​", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2020/10/harraz-30-pcs.jpg" , price: "6,500", category_id: 1 )
+Product.create(name: "12 Pcs Signature Cookware Set", description: "Suitable to cook any recipe and selected to handle a large range of kitchen tasks. Exceptionally durable dishwasher safe cookware for easy cleanup. Durable non-stick interior for easy cleaning and cooking. The set is superior scratch resistance and has a total non-stick interior that provides high performance and cooking experience. Designed for high performance and durability, but also to look great in any kitchen. Ergonomic comfortable grip.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/wawaw.jpg" , price: 6500, category_id: 1 )
+Product.create(name: "3-tier fruit bowl/ candy bowl/ Snack bowl", description: "Ceramic candy dish living room home three-layer fruit plate snack plate creative modern dried fruit fruit basket.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/3-tier-fruit-bowl-candy-bowl-Snack-bowl-1.jpg" , price: 4500, category_id: 1 )
+Product.create(name: "Always Stainless Steel Vacuum Flask", description: "Stainless steel insulation hence unbreakable.Durable and easy to wash.Easy to carry anywhere & you can use it to serve guests or anyone.Perfect to use in offices, homes, restaurants, schools, churches etc.The flask can also be used to keep hot anything-Hot chocolate, coffee, tea, Uji etc.The flask keeps items hot  for more than 24 hours.The product is a guarantee customer satisfaction", image: "https://decorfinity.co.ke/wp-content/uploads/2021/03/Flask-2-e1614669189372.jpg" , price: 2500, category_id: 1 )
+Product.create(name: "Bosch Cookware",description: "Since It’s Non-Stick, You get to use less oil – this means healthier meals – when you cook with the Bosch Non-stick Cooking Pots, the granite coating does not come out when heated or gently scratched.", image: "https://goldstarnovelty.co.ke/wp-content/uploads/2022/03/1-1-600x600-1.png" , price: 10500, category_id: 1 )
+Product.create(name: "9PCs Knife Set-Stainless Steel", description: "These knives will do wonders for your life. Stored on a gorgeous acrylic stand and equipped with ergonomic soft touch handles, this knife sets are a beautiful compliment to any kitchen ", image: "https://decorfinity.co.ke/wp-content/uploads/2022/05/9-13-600x800.jpg" , price: 1500, category_id: 1 )
+Product.create(name: "6pc Coffee Mug Set With Bamboo Stand", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/cpp.jpg" , price: 3000, category_id: 1 )
+Product.create(name: "30 Pcs Stainless Steel Non-Stick Cookware Set​", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2020/10/harraz-30-pcs.jpg" , price: 6500, category_id: 1 )
 Product.create(name: "", description: "Non-Stick and Durable with guaranteed durability, Non-stick interior for easy cleaning and cooking
 The set has superior scratch resistance and has a totally non-stick interior that provides high performance and a sleek cooking experience.
 Get this wide range of cooking tools, helping you prepare meals for your family to taste better and in less time
-Maximizes heat absorption with the conductivity surface to ensure your food cooks faster and more evenly.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/wawaw.jpg" , price: "12,500", category_id: 1 )
-Product.create(name: "34 Eggs Tray Holder with Lid", description: "Smooth and arc edge design, will not cut hand and egg.", image: "" , price: "1,500", category_id: 1 )
-Product.create(name: "4 Set Of Reusable Gas Hob Protector", description: "These gas hob protectors are 100% non-stick, can be easily adjusted for a perfect fit and will stay in place and allows Teflon protective sheets to be easily removed when cleaning.", image: "https://decorfinity.co.ke/wp-content/uploads/2020/11/Reusable-Gas-Hob-Protector-1.jpg" , price: "1,500", category_id: 1 )
+Maximizes heat absorption with the conductivity surface to ensure your food cooks faster and more evenly.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/wawaw.jpg" , price: 12500, category_id: 1 )
+Product.create(name: "34 Eggs Tray Holder with Lid", description: "Smooth and arc edge design, will not cut hand and egg.", image: "" , price: 1500, category_id: 1 )
+Product.create(name: "4 Set Of Reusable Gas Hob Protector", description: "These gas hob protectors are 100% non-stick, can be easily adjusted for a perfect fit and will stay in place and allows Teflon protective sheets to be easily removed when cleaning.", image: "https://decorfinity.co.ke/wp-content/uploads/2020/11/Reusable-Gas-Hob-Protector-1.jpg" , price: 1500, category_id: 1 )
 Product.create(name: "6 Lts Commercial Deep Fryer", description: "Heavy-duty stainless steel construction.
 Thermostats for accurate temperature setting.
 Boil lids to prevent oil splashing.
 Restaurants, supermarkets, fast food stands, snack bars, parties, etc. It is perfect for commercial uses such as.
-Suitable for buffalo wings, chicken, meatballs, EGGSrolls, shrimp rolls, mozzarella sticks, jalapeno poppers, etc.", image: "https://decorfinity.co.ke/wp-content/uploads/2020/09/6-Lts-Commercial-Deep-Fryer.jpg" , price: "5,500", category_id: 1 )
+Suitable for buffalo wings, chicken, meatballs, EGGSrolls, shrimp rolls, mozzarella sticks, jalapeno poppers, etc.", image: "https://decorfinity.co.ke/wp-content/uploads/2020/09/6-Lts-Commercial-Deep-Fryer.jpg" , price: 5500, category_id: 1 )
+Product.create(name: "3 Pcs Metallic Trouser Hangers", description: "Your closet lifesaver. This excellent S-shaped hanger will Maximize to save your closet space and keep it clean and orderly. It can be used for hanging clothes, pants, tie, scarfs, belt and towel.
+Rust Resistance. High-quality stainless steel material made it rust-free, so you could also use it in the bathroom.
+Durable. The hanger has high hardness and intensity that make it won’t be easy to out of shape.
+Super convenience to dry your clothes, pants and scar", image: "https://decorfinity.co.ke/wp-content/uploads/2020/10/3-Pcs-Metallic-Trouser-Hangers-600x600.jpg" , price: 1500, category_id: 2 )
+Product.create(name: " Washing machine rack balcony laundry cabinet", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2020/11/Washing-Machine-Rack-With-Balcony-Laundry-Cabinet-1.jpg" , price: 3700, category_id: 2 )
+Product.create(name: "Tissue holder, Pink, White and Black", description: "Tissue holder, Pink, White and Black", image: "https://dazzlingdecor.co.ke/wp-content/uploads/2021/02/6.jpg" , price: 1200, category_id: 2 )
+Product.create(name: "Dressing Mirror", description: "Dressing mirror design features a rectangular shape supported by a floor stand", image: "https://decorfinity.co.ke/wp-content/uploads/2020/11/Dressing-Mirror-new-719x510.jpeg" , price: 3000, category_id: 2 )
+Product.create(name: "3 Tier Candy shower", description: ".", image: "https://decorfinity.co.ke/wp-content/uploads/2022/10/1000011574-790x510.jpg" , price: 1800, category_id: 2 )
+Product.create(name: "4- tier movable Trolley White and Black", description: "", image: "https://dazzlingdecor.co.ke/wp-content/uploads/2021/02/8.jpg" , price: 4000, category_id: 2 )
+Product.create(name: "Clothes Drying Rack", description: "Material:Stainless Steel Metal + ABS.Approximately measure: 20″x13.7″x6.2″No installation necessary!
+The hook-on drying rack can be hung on any surface of up to 16cm thickness.
+It is rust proof and heat resistant, able to withstand prolonged exposure in the sun without cracking.
+It can hold up to 5kg.Multifunction for using in Bathroom, Windowsill, Guardrail, Corridor, Balcony etc.", image: "https://skygarden.azureedge.net/media/products/759474-826e6943942244dd837216167093a994.jpg" , price: 3600, category_id: 2 )
+Product.create(name: "Anti Slip Bathroom Mat", description: "Safe contemporary bathroom mat
+Durable, anti slip resistant construction
+Easy to wash by hand.
+Leave in a ventilated place to dry
+Trendy
+Durable
+Beautiful
+Easy to clean", image: "https://dazzlingdecor.co.ke/wp-content/uploads/2021/02/Anti-Slip-Bathroom-Mat-600x600-1.jpg" , price: 900, category_id: 2 )
+
+
+Product.create(name: "Advanced Fitness Exercise Spin Bike", description:  "Multifunction LCD display
+Heavy-duty precision cast iron flywheel
+Heavy-duty strong steel frame
+Adjustable resistance control
+Automotive-grade belt
+Adjustable handlebar
+Racing seat design for performance
+Recommended Weight Limit: 120kg", image: "https://decorfinity.co.ke/wp-content/uploads/2021/12/images.jpg" , price: 35000 , category_id: 5 )
+Product.create(name: "4 in 1 Tummy Trimmer Fitness Training kit", description: "1 High quality Hand Grip for wrist and hand workouts
+1 Tummy Trimmer, Pull exerciser: For tummy trimming and belly fat burning.
+1 Wooden Handle Skipping Rope For warming up
+1 chest pull exerciser
+keep fit and keeping ill health away
+Firms and tones legs bums and tummy
+Impact free motion
+Aerobic conditioning as well as strength training
+Tightens abdominals‎
+Strengthens back muscles ‎.‎
+Easy to use and store", image: "https://decorfinity.co.ke/wp-content/uploads/2020/11/4-in-1-Tummy-Trimmer-Fitness-Training-kit.jpeg" , price: 2500, category_id: 5 )
+Product.create(name: "Chrome dumbbells set", description: "Premium 15 KG/20KG/30KG/50KG Chrome Dumbbell Set in sturdy plastic carry / storage case
+Includes full color exercise wall chart
+Ideal for upper body works outs and toning exercises
+Attractive chrome finish stands out from traditional cast or vinyl weights
+Set comprises of 4 x 0.5 KG and 8 x 1.25 KG Chrome Weight Plates, 2 x Chrome Spinlock bars with ergo rubber grips, and 4 x Spinlock collars
+", image: "https://d2j6dbq0eux0bg.cloudfront.net/images/40569255/3113076783.jpg" , price: 15000 , category_id: 5 )
+Product.create(name: "Crystal Semi-Commercial Spinning Bike", description: "", image: "https://www.powermaxfitness.net/uploads/1675752126_product_07022023121206.jpg" , price: 35000, category_id: 5 )
+Product.create(name: "", description: "High-quality steel stabilizes the bike.
+2-way(up/down)adjustable handles and 4-way(front/back/up/down) adjustable seat.
+Seat height adjustment range: 0~5.91 inches.
+Applicable height: 4 feet 11 inches ~ 6 feet 3 inches.
+Handlebar height adjustment range: 0~2.76 inches.
+Heartrate tracking on handles.
+Accurate welding – zero worries of wobbling.
+Stable and quiet belt gearing.
+18kgs flywheel.
+Anti-slip cage pedals to keep you safe.
+Can bear up to 150kgs.", image: "https://decorfinity.co.ke/wp-content/uploads/2021/02/58dfb88d-3693-4b1e-8d12-293748d78bba-878x1024.jpg" , price: 45000 , category_id: 5 )
+Product.create(name: "Exercise Treadmill", description: "Inclusive of a Sit-up bar, Leg and Waist Trainer Belt and a Massager.
+Track your Speed, Time, Distance and Calories burnt.
+Pulse Sensors track your Heart Rate – know when to speed up/slow down.
+Continuous Speed Adjustment Knob.
+12 different Variable Workout Intensity levels (pre-programmed).
+Digital Control Panel with a 3.2inch LCD Display
+Max user weight : 130 kgs.
+Max Speed 12KM/H
+Input Voltage: 220v-240v.
+Product Size: 1450×675×1160mm.
+Folding Size: 970×675×1275mm", image: "https://cdn.shopify.com/s/files/1/0554/8795/5111/products/Tread_T30_XIR_hero.png?v=1673897411" , price: 85000 , category_id: 5 )
 
 
