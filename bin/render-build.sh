d91cefc21c77a07fa07e5c98c8e58d3648b12ac0
Zack <ndunguzachary24@gmail.com>
2024-01-25 23:04:23 +0300

    Intergrated migrate and seed on deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
-# bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
